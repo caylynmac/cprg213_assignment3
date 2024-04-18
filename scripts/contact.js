e18b0d5e1031be81a0f09a1771508eeb0e711add
caylynmac <152448716+caylynmac@users.noreply.github.com>
2024-04-18 06:20:17 -0600

    Add files via upload

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,11 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+let submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", changePage);
+let page = document.getElementById("contact-page");
+
+function changePage() {
+    page.innerHTML="<p>Thank you for your message!</p>";
+    page.style.fontSize = 24;
+  }
